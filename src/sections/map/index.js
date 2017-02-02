0b973dd3c07abe4b6c0dd7ddbe9ad676058f1cc0
Chris Sargent <chris@stickypixel.com>
2017-02-02 17:00:42 +0100

    Contact us margins etc

--- src/sections/map/index.js
+++ src/sections/map/index.js
@@ -8,7 +8,7 @@ const Gmap = ({compName, options, map, map_link}) => {
 
   return (
     <div className={css.main + compName}>
-      <Vcard {...options} modifier='compact' pin/>
+      <Vcard {...options} pin/>
       <a href={map_link} className="map" target="_blank">
         <RespImageCover image={map} />
       </a>
