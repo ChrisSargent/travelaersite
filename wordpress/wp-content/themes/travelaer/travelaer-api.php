9588e7e6908cafddb0b27a22f5f32c051d5bc973
Chris Sargent <chris@stickypixel.com>
2017-03-21 12:25:05 +0100

    Pre-release

--- wordpress/wp-content/themes/travelaer/travelaer-api.php
+++ wordpress/wp-content/themes/travelaer/travelaer-api.php
@@ -255,9 +255,8 @@ function travelaer_get_the_content_by_id($post_id)
 {
     $page_data = get_page($post_id);
     if ($page_data) {
-        wlog($page_data);
-        return wpautop($page_data->post_content);
-        // return apply_filters('the_content', $page_data->post_content);
+      // return wpautop($page_data->post_content);
+      return apply_filters('the_content', $page_data->post_content);
     } else {
         return false;
     }
