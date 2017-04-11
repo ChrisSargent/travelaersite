737ddc003e2709ce75bf095a99c84c44dd574e71
Chris Sargent <chris@stickypixel.com>
2017-04-11 19:26:40 +0200

    Changing logging - no release

--- config/webpack.config.server.js
+++ config/webpack.config.server.js
@@ -2,15 +2,11 @@ var fs = require('fs');
 var path = require('path');
 var paths = require('./paths');
 var webpack = require('webpack');
-
 var getClientEnvironment = require('./env');
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
 var publicPath = paths.servedPath;
-// Some apps do not use client-side routing with pushState.
-// For these, "homepage" can be set to "." to enable relative asset paths.
-var shouldUseRelativeAssetPaths = publicPath === './';
 // `publicUrl` is just like `publicPath`, but we will provide it to our app
 // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
 // Omit trailing slash as %PUBLIC_URL%/xyz looks better than %PUBLIC_URL%xyz.
@@ -65,6 +61,13 @@ module.exports = {
         use: 'ignore-loader'
       }
     ]
+  },
+
+  resolve: {
+    alias: {
+      // Because we're in Node environment, use raven, not raven-js
+      'raven-js': 'raven'
+    }
   },
 
   plugins: [
--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const app = express()
 const PORT = 8085
 
 // Raven is for error monitoring
-Raven.config('https://51a14cb683344ad1b2f1b64d037d8d88:a1ec292179804b00ac85408031c13d84@sentry.io/156925').install()
+Raven.config('https://51a14cb683344ad1b2f1b64d037d8d88:a1ec292179804b00ac85408031c13d84@sentry.io/156925', {release: process.env.PACKAGE.version, logger: 'node'}).install()
 app.use(Raven.requestHandler())
 app.use('/assets', express.static('build/assets'))
 app.use('/static', express.static('build/static'))
