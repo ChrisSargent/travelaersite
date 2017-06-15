91ce6fea7e0c9a1cfaf3feecee15f63f88db0f41
Chris Sargent <chris@stickypixel.com>
2017-06-15 23:40:13 +0200

    Updating backup script

--- config-server/backups.sh
+++ config-server/backups.sh
@@ -1,39 +1,50 @@
 #!/bin/bash
 ####################################
 #
-# Backup Locally.
+# Variables.
 #
 ####################################
 
 # What to backup.
 backup_files="/var/www/html"
 
-# Where to backup to.
+# Local backup files folder
 dest="/home/travelaer/backups/files"
+
+# Backup Folder
+backup_folder="/home/travelaer/backups/"
 
 # Create archive filename.
 day=$(date +%m%d%y)
 hostname=$(hostname -s)
 archive_file="$hostname-$day.tgz"
 
-# Print start status message.
-echo "Backing up $backup_files to $dest/$archive_file"
-date
-echo
+####################################
+#
+# Backup Locally.
+#
+####################################
 
-# Backup the files using tar.
-tar czvpf $dest/$archive_file $backup_files
-
-# Delete files older than 7 days
+echo "Backing up $backup_files to $dest/$archive_file..."
+tar czpf $dest/$archive_file $backup_files
+echo "Local backup finished on $(date)"
 echo
 echo "Deleting backups older than 7 days..."
 find /home/travelaer/backups/files* -mtime +7 -exec rm {} \;
-
-# Print end status message.
-echo "Backup finished"
+echo "Finished deleting old backups on $(date)"
 echo
-
-date
-
-# Long listing of files in $dest to check file sizes.
 ls -lh $dest
+echo
+echo "** Local Backup Complete.**"
+
+####################################
+#
+# Backup Remotely.
+#
+####################################
+echo
+echo "Syncing with AWS..."
+aws s3 sync $backup_folder s3://travelaer-backup-website --delete --only-show-errors
+echo "Remote backup finished on $(date) "
+echo
+echo "** Backup complete. **"
